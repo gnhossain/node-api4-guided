dfb525f2c4ff1c70fb40a7cbd403fbb6341b6f37
gul hossain <spicymangojungle@gmail.com>
2020-04-09 23:44:32 -0700

    fixed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require("./api/server.js");
 
 // make the port dynamic, so heroku can assign one
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
